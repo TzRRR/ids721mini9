a8c829c6f2052b5d3b8674ae2d0cabff48bd6156
Tz <r503114568@163.com>
2025-03-28 16:17:53 -0400

    read secret

--- huggingface_api.py
+++ huggingface_api.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-HF_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
+HF_TOKEN = st.secrets["HUGGINGFACE_API_TOKEN"]
 
 client = InferenceClient(
     provider="hyperbolic",
